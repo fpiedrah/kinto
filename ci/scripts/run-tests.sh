50d7ea11e7f893919b73b2411cad9f4884590d60
Francisco J. Piedrahita <fpiedrah93@gmail.com>
2019-07-30 16:43:49 -0500

    Added apt install to file

--- ci/scripts/run-tests.sh
+++ ci/scripts/run-tests.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-apt-get update -y && apt install -y build-essential python-pip
+apt-get update -y && apt install -y build-essential python3-pip
 
+pwd
 ls -al
 #make ../../install-dev
 #make ../../test
